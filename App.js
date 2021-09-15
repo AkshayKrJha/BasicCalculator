e0948c9bd3852f0e581c416f11df30381ed7f8d6
Akshay Kumar Jha <akshay@aclize.com>
2021-09-15 22:05:40 +0530

    Using TouchableOpacity for Button and flexDirection to row sets buttons in horizontal direction

--- App.js
+++ App.js
@@ -6,20 +6,103 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React, { useState } from 'react';
 import {
+  TouchableOpacity,
   Button,
+  FlatList,
+  Text,
   TextInput,
   View,
 } from 'react-native';
 
+const add = (output, input) => {
+  return output + input;
+}
+
+const subtract = (output, input) => {
+  return output - input;
+}
+
+const multiply = (output, input) => {
+  return output * input;
+}
+
+const divide = (output, input) => {
+  return output / input;
+}
+
+const result = (output, operand, input) => {
+  switch (operand) {
+    case '*':
+      return multiply(output, input);
+      break;
+    case '/':
+      return divide(output, input);
+      break;
+    case '+':
+      return add(output, input);
+      break;
+    case '-':
+      return subtract(output, input);
+      break;
+  }
+}
+
+const CalculativeButton = (property) => {
+  return (
+    <View style={{ flex: 1 }}>
+      <TouchableOpacity>
+        <Text>{property.value}</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
 const Calculator = () => {
   //textIput
-  return (<View>
-    <TextInput style={{ height: '50%', width: '100%', backgroundColor: 'skyblue', borderBottomColor: 'red', borderBottomWidth: 4 }}>
+  //find current character typed
+  const operand = ['*', '/', '+', '-'];
+  const [currentResult, SetCurrentResult] = useState(0);
+  const [currentOperand, SetCurrentOperand] = useState('+');
+  const [currentInput, SetCurrentInput] = useState(0);
+  return (
+    <View>
+      <Text style={textStyle()}>
+        {currentInput}
+      </Text>
+      <View style={{ flexDirection: 'row' }}>
+        <CalculativeButton value='0' />
+        <CalculativeButton value='1' />
+        <CalculativeButton value='2' />
+        <CalculativeButton value='3' />
+        <CalculativeButton value='4' />
+      </View>
+      <View style={{ flexDirection: 'row' }}>
+        <CalculativeButton value='5' />
+        <CalculativeButton value='6' />
+        <CalculativeButton value='7' />
+        <CalculativeButton value='8' />
+        <CalculativeButton value='9' />
+      </View>
+      <View style={{ flexDirection: 'row' }}>
+        <CalculativeButton value='+' />
+        <CalculativeButton value='-' />
+        <CalculativeButton value='*' />
+        <CalculativeButton value='/' />
+        <CalculativeButton value='=' />
+      </View>
+    </View>);
+}
 
-    </TextInput>
-  </View>);
+const textStyle = () => {
+  return {
+    height: '25%',
+    width: '100%',
+    backgroundColor: '#dddddd',
+    borderBottomColor: '#222222',
+    borderBottomWidth: 3
+  }
 }
 
 export default Calculator;
